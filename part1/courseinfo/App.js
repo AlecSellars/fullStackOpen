6bfd7c7743a7c3e8cfbcc833b16f60b3694536a9
Alec Sellars <alecsellars@gmail.com>
2023-04-20 19:10:16 -0700

    Update App.js

--- part1/courseinfo/App.js
+++ part1/courseinfo/App.js
@@ -49,7 +49,7 @@ const Content = (props) => {
 const Totals = (props) => {
   return (
     <p>
-      Number of exercises{" "}
+      Number of exercises
       {props.parts[0].exercises +
         props.parts[1].exercises +
         props.parts[2].exercises}
